769c34058dbff72fd9ee488ffb8800d6017a13aa
Harry134915 <hairuike35@gmail.com>
2026-01-24 17:17:06 +0800

    优化

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -12,9 +12,9 @@ namespace Login_Pape
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ButtonState == MouseButtonState.Pressed)
+            if (e.ChangedButton == MouseButton.Left)
             {
-                DragMove();
+                this.DragMove();
             }
         }
     }
